a359b8e558212d49343f784d48e6dfda7469898e
xilinlian <xilin.lian.sisi@gmail.com>
2022-09-19 10:23:59 +0800

    refactor: extract mockData

--- src/http/test/http.test.js
+++ src/http/test/http.test.js
@@ -1,26 +1,23 @@
-import axios from 'axios';
-import {getData} from '../http';
+import axios from "axios";
+import { getData } from "../http";
+import PRODUCTS from "../../mockData/products.json";
 
-jest.mock('axios');
+jest.mock("axios");
 
-describe('http', function () {
-  describe('getData', function () {
-    it('should send request and return data', async function () {
+describe("http", function () {
+  describe("getData", function () {
+    it("should send request and return data", async function () {
       let mockData = {
-        products: [
-          {'id': 1, 'name': '小米手环', 'price': 299.00, 'count': 1},
-          {'id': 2, 'name': '任天堂 Nintendo Switch', 'price': 2099.00, 'count': 2},
-          {'id': 3, 'name': 'SONY WH-1000XM4 无线智能降噪耳机', 'price': 2099.00, 'count': 1},
-          {'id': 4, 'name': 'iPhone 13 256GB', 'price': 6799.00, 'count': 1}
-        ],};
+        products: PRODUCTS,
+      };
 
       axios.get.mockResolvedValue({
-        data: mockData
+        data: mockData,
       });
 
       const actual = await getData();
 
-      expect(axios.get).lastCalledWith('http://127.0.0.1:8000/');
+      expect(axios.get).toBeCalledWith("http://127.0.0.1:8000/");
 
       expect(actual).toEqual(mockData);
     });
--- src/service/test/index.test.js
+++ src/service/test/index.test.js
@@ -1,18 +1,14 @@
-import {getData} from '../../http/http';
-import {getProducts} from '../index';
+import { getData } from "../../http/http";
+import { getProducts } from "../index";
+import PRODUCTS from "../../mockData/products.json";
 
-jest.mock('../../http/http');
+jest.mock("../../http/http");
 
-describe('service', function () {
-  describe('getProducts', function () {
-    it('should return products', async function () {
-      const mockProducts = [
-        {'id': 1, 'name': '小米手环', 'price': 299.00, 'count': 1},
-        {'id': 2, 'name': '任天堂 Nintendo Switch', 'price': 2099.00, 'count': 2},
-        {'id': 3, 'name': 'SONY WH-1000XM4 无线智能降噪耳机', 'price': 2099.00, 'count': 1},
-        {'id': 4, 'name': 'iPhone 13 256GB', 'price': 6799.00, 'count': 1}
-      ];
-      getData.mockResolvedValue({products: mockProducts});
+describe("service", function () {
+  describe("getProducts", function () {
+    it("should return products", async function () {
+      const mockProducts = PRODUCTS;
+      getData.mockResolvedValue({ products: mockProducts });
       const result = await getProducts();
 
       expect(result).toEqual(mockProducts);
